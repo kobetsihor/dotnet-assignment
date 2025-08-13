ef5736aa21b2e44495b301a6e2032d0e776f235b
Ihor Kobets <kobets.ihor@gmail.com>
2025-08-13 18:02:35 +0100

    Added validator for GetVetAppointmentsRequest

---
+++ Calendar/Calendar.Api/Validation/GetVetAppointmentsRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Calendar.Contracts.Requests;
+using FluentValidation;
+
+namespace Calendar.Api.Validation
+{
+    public class GetVetAppointmentsRequestValidator : AbstractValidator<GetVetAppointmentsRequest>
+    {
+        public GetVetAppointmentsRequestValidator()
+        {
+            RuleFor(x => x.StartDate)
+                .LessThanOrEqualTo(x => x.EndDate)
+                .WithName(nameof(GetVetAppointmentsRequest.StartDate))
+                .WithMessage($"{nameof(GetVetAppointmentsRequest.StartDate)} must be before or equal to {nameof(GetVetAppointmentsRequest.EndDate)}.");
+
+            RuleFor(x => x.EndDate)
+                .GreaterThanOrEqualTo(x => x.StartDate)
+                .WithName(nameof(GetVetAppointmentsRequest.EndDate))
+                .WithMessage($"{nameof(GetVetAppointmentsRequest.EndDate)} must be after or equal to {nameof(GetVetAppointmentsRequest.StartDate)}.");
+        }
+    }
+}
